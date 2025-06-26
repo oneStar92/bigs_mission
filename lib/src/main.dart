ba1d6015266aa46777271a5a91c7a003e5866f54
Ayaan <onestar7984@gmail.com>
2025-06-26 17:25:30 +0900

    refactor: 세로모드 고정

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,10 +1,14 @@
 import 'package:bigs/src/core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await TokenStorage.instance.load();
   DioClient.instance.initial();
 
